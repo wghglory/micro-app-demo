1a6947b0da5d9869d59fdb8ad65497deac023bab
Guanghui Wang <guanghuiw@vmware.com>
2021-09-04 14:15:54 +0800

    feat: add headers in child react app

--- children/child-react/config/webpackDevServer.config.js
+++ children/child-react/config/webpackDevServer.config.js
@@ -16,6 +16,9 @@ const sockPort = process.env.WDS_SOCKET_PORT;
 
 module.exports = function (proxy, allowedHost) {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
     // https://github.com/webpack/webpack-dev-server/issues/887
@@ -32,8 +35,7 @@ module.exports = function (proxy, allowedHost) {
     // So we will disable the host check normally, but enable it if you have
     // specified the `proxy` setting. Finally, we let you override it if you
     // really know what you're doing with a special environment variable.
-    disableHostCheck:
-      !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
+    disableHostCheck: !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     // Enable gzip compression of generated files.
     compress: true,
     // Silence WebpackDevServer's own logs since they're generally not useful.
